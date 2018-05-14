b2199687a352c9031d0a6222ca6a114a05795985
snekw <dev.snekw@gmail.com>
2018-05-14 03:37:32 +0300

    Trying to get luaObjectToJson to work on linux

--- lua/getLuaObject.js
+++ lua/getLuaObject.js
@@ -1,31 +1,33 @@
 /**
- *  wfData,
+ *  wfApi,
  *  Copyright (C) 2017 Ilkka Kuosmanen
  *
- *  This file is part of wfData.
+ *  This file is part of wfApi.
  *
- *  wfData is free software: you can redistribute it and/or modify
+ *  wfApi is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  wfData is distributed in the hope that it will be useful,
+ *  wfApi is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with wfData.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with wfApi.  If not, see <http://www.gnu.org/licenses/>.
  */
 'use strict';
 const spawn = require('child_process').spawn;
 function getData (input) {
   return new Promise((resolve, reject) => {
     let luaCmd = 'lua5.3';
+    let luaParams = ['.', './luaObjectToJson.lua'];
     if(process.platform === "win32"){
-      luaCmd = './lua/lua_bin/lua53.exe';
+      luaCmd = './lua/lua53.exe';
+      luaParams = ['./lua/luaObjectToJson.lua'];
     }
-    let luaToJson = spawn(luaCmd, ['./lua/luaObjectToJson.lua']);
+    let luaToJson = spawn(luaCmd, luaParams);
 
     luaToJson.stderr.pipe(process.stderr);
     luaToJson.stdin.setEncoding('utf-8');
